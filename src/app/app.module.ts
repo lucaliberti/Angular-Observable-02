8985bb96ab5369e6087a2bfcde35b9a54ce0ba86
lucaliberti <liberti.luca@gmail.com>
2022-02-15 21:55:01 +0100

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Observable01Component } from './observable01/observable01.component';
-import { Observable02Component } from './observable02/observable02.component';
-import { Observable03Component } from './observable03/observable03.component';
-import { Observable04Component } from './observable04/observable04.component';
-import { Observable05Component } from './observable05/observable05.component';
 //import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Observable01Component,
-    Observable02Component,
-    Observable03Component,
-    Observable04Component,
-    Observable05Component,
-   
+    Observable01Component,   
   ],
   imports: [
     BrowserModule
--- src/app/observable01/observable01.component.ts
+++ src/app/observable01/observable01.component.ts
@@ -1,35 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { of, Observable, Observer } from 'rxjs';   // importare questo modulo
+import { Component, OnInit } from "@angular/core";
+import { interval, Observable, Subscription, Observer, VirtualTimeScheduler } from "rxjs"; // importare questo modulo
 
 @Component({
-  selector: 'app-observable01',
-  templateUrl: './observable01.component.html',
-  styleUrls: ['./observable01.component.css']
+  selector: "app-observable01",
+  templateUrl: "./observable01.component.html",
+  styleUrls: ["./observable01.component.css"],
 })
 
+
 export class Observable01Component implements OnInit {
+  counter1:number=0
+  counter2:number=0
+  mySubcription1:Subscription
+  mySubcription2:Subscription
 
+  myObservable1: Observable<number>;
 
-
-  // "of" emette una sequeza di valori, alla fine emette la notifica "complete" 
-  // of restituisce un observable 
-  myObservable: Observable<Object> = of('x1', 'x2', 'x3');
-
-  myObserver = {
-    next:     (x: string)   => console.log('Observer1: ho ricevuto il dato: ' + x),
-    error:    (err: string) => console.error('Observer1: ho ricevuto un errore: ' + err),
-    complete: ()            => console.log('Observer1: ho ricevuto un complete. Bye'),
+  myObserver1 = {
+    next: (x: number) => { 
+      console.log("Observer1: ho ricevuto il dato: " + x)
+      this.counter1=x
+    },
+    error: (err: string) => console.error("Observer1: ho ricevuto un errore: " + err),
+    complete: () => console.log("Observer1: sequenza completata"),
   };
-
  
 
-  makeSubscription(): void {
-    // http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-subscribe
-    // restituisce un oggetto ISubscription
-    this.myObservable.subscribe(this.myObserver);
+
+  constructor() {}
+
+  ngOnInit() {
+    this.myObservable1 = interval(1000)
   }
 
-  constructor() { }
-  ngOnInit() { }
+  onSubscribe1(){
+    this.mySubcription1=this.myObservable1.subscribe(this.myObserver1);
+  }
 
+  onSubscribe2(){
+    this.mySubcription2=this.myObservable1.subscribe(
+      (x: number) => { 
+        console.log("Observer2: ho ricevuto il dato: " + x)
+        this.counter2=x
+      }
+    )
+  }
+
+
+  onUnsuscription1(){
+    this.mySubcription1.unsubscribe()
+  }
+
+  onUnsuscription2(){
+    this.mySubcription2.unsubscribe()
+  }
 }
